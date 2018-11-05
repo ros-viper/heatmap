128881e0637f84eebc6f6dbc2886392b3e76edd4
Rostyslav Rzhenetskyy (1000059037) <RZHER1@student.op.ac.nz>
2018-11-05 14:10:50 +1300

    Latest build

--- front-end/src/components/map/map.jsx
+++ front-end/src/components/map/map.jsx
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 import './map.css';
 import * as d3 from 'd3';
 import ReactTooltip from 'react-tooltip';
+import { Tooltip } from 'react-tippy';
+import 'react-tippy/dist/tippy.css';
 
 
 const mapStateToProps = state => {
@@ -19,6 +21,8 @@ class ConnectedMap extends Component {
 
         this.click = this.click.bind(this);
         this.hover = this.hover.bind(this);
+        this.getCoord = this.getCoord.bind(this);
+        this.drawMap = this.drawMap.bind(this);
     }
 
     click(data) {
@@ -29,10 +33,12 @@ class ConnectedMap extends Component {
         console.log(data);
     }
 
-    componentDidUpdate() {
-       d3.select("#map").select("svg").remove();
+    getCoord(data) {
+        console.log(data.nativeEvent.offsetX);
+    }
 
-       const sensors = this.props.sensors.filter(sensor => sensor.floor === this.props.floor);
+    drawMap() {
+        const sensors = this.props.sensors.filter(sensor => sensor.floor === this.props.floor);
 
         const svg = d3.select("#map")
                     .append("svg")
@@ -59,50 +65,32 @@ class ConnectedMap extends Component {
         ReactTooltip.rebuild()
     }
 
+    componentDidUpdate() {
+        d3.select("#map").select("svg").remove();
+        this.drawMap();
+    }
+
     componentDidMount() {
-
-        const sensors = this.props.sensors.filter(sensor => sensor.floor === this.props.floor);
-
-        const svg = d3.select("#map")
-                    .append("svg")
-                    .attr("width", 1140)
-                    .attr("height", 850);
-
-        const circles = svg
-                            .selectAll("circle")
-                            .data(sensors)
-                            .enter()
-                            .append("a")
-                            .attr("data-for", function(d) {return d.serialID;})
-                            .attr("data-tip", "")
-                            .append("circle");
-
-        const circleAttributes = circles
-                                    .attr("cx", function(d) {return d.x;})
-                                    .attr("cy", function(d) {return d.y;})
-                                    .attr("r", 30)
-                                    .attr("fill", function(d) {return d.color;})
-                                    .on("click", (d) => {this.click(d);})
-                                    .on("mouseover", (d) => {this.hover(d);});
-        // Rebind tooltips after trigerring anchors are added by D3
-        ReactTooltip.rebuild()
-        
+        this.drawMap();
     }
 
     render() {
         const sensors = this.props.sensors.filter(sensor => sensor.floor === this.props.floor);
         return (
             <div>
-                <div id="map" className={this.props.floor.toString()}></div>
-                
-                {sensors.map((sensor, index) => (
-                    <ReactTooltip key={index} id={sensor.serialID.toString()} type='info' effect='solid'>
-                        <span className='reading'>ID: {sensor.serialID}</span>
-                        <span className='reading'>Temperature: {sensor.temperature}</span>
-                        <span className='reading'>Humidity: {sensor.humidity}</span>
-                        <span className='reading'>Luminosity: {sensor.luminosity}</span>
-                    </ReactTooltip>
-                ))}
+                <Tooltip trigger="click" title="Welcome to React" followCursor={true}>
+                    <div id="map" className={this.props.floor.toString()} onClick={this.getCoord}></div>
+                    
+                    {sensors.map((sensor, index) => (
+                        <ReactTooltip key={index} id={sensor.serialID.toString()} type='info' effect='solid'>
+                            <span className='reading'>ID: {sensor.serialID}</span>
+                            <span className='reading'>Temperature: {sensor.temperature}</span>
+                            <span className='reading'>Humidity: {sensor.humidity}</span>
+                            <span className='reading'>Luminosity: {sensor.luminosity}</span>
+                        </ReactTooltip>
+                    ))}
+    
+                </Tooltip>
             </div>
         );
     }
