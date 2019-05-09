f601d93f923c9333becb2e69656005f8b8ca996c
Rostyslav Rzhenetskyy <rzher1@student.op.ac.nz>
2019-05-09 14:16:55 +1200

    Testing refresh

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from api import views
 
 urlpatterns = [
     path('sensors/', views.sensor_list),
-    path('sensors/<int:pk>', views.sensor_details),
+    path('sensors/<int:pk>/', views.sensor_details),
     path('history/', views.history_list),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>', views.UserDetail.as_view()),
